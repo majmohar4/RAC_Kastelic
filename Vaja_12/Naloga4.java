470de1a251c622cafdce3d77d3fd1659faabf6ba
majmohar4 <maj.mohar4@gmail.com>
2025-01-03 13:30:59 +0100

    Automatski commit: 2025-01-03 13:30

---
+++ Vaja_12/Naloga4.java
@@ -0,0 +1,51 @@
+import java.lang.*;
+import java.util.*;
+/**
+ * Opis: 
+ * 
+ * @author Maj
+ * @version 18. 12. 2024
+ */
+public class Naloga4 {
+	public static void main(String[] args) {
+		char[] t = {'B','D','F','G','L','M','P','S','T','V','Y','Z'};
+		char crka = generirajCrko();
+		int index = poisciCrkoLinearno(crka, t);
+		if(index==-1)System.out.println("Črke "+crka+" nismo našli v tabeli.");
+		else System.out.println("Črko "+crka+" smo v tebeli našli na indexu "+index+".");
+	}
+	public static char generirajCrko(){
+		char[] crke = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+		char a = crke[(int)(Math.random()*26)];
+		return a;
+	}
+	public static int poisciCrkoBinarno(char iskano, char[] tab){
+		int zac = 0, kon = tab.length-1, sre,a=0, ret=-1;
+		do {
+			sre = (zac+kon)/2;
+			if (tab[sre] == iskano){
+				ret=sre;
+				break;
+			}
+			else if (tab[sre]<iskano){
+				zac = sre+1;
+			}
+			else kon = sre-1;
+			a++;
+		}while(zac<=kon);
+	System.out.println("primerjali smo "+a+"-krat");
+	return ret;
+	}
+	public static int poisciCrkoLinearno(char crka, char[] tab){
+		int a =0, ret=-1;
+		for (int i =0; i<tab.length; i++){
+			a++;
+			if (tab[i] == crka){
+				ret= i;
+				break;
+			}
+		}
+		System.out.println("primerjali smo "+a+"-krat");
+		return ret;
+	}
+}
---
+++ Vaja_12/Vizualizacija00.java
@@ -0,0 +1,48 @@
+import java.lang.*;
+import java.util.*;
+/**
+ * Opis: 
+ * 
+ * @author Maj
+ * @version 18. 12. 2024
+ */
+public class Vizualizacija00 {
+	
+	static int dolzinaTabele=12;
+	
+	public static void main(String[] args){
+		
+		int[] tab=generirajTabelo(dolzinaTabele);
+		
+		izpisiPrvoVrsticoTabele(tab);
+		izpisiKazelecElemTabele(tab,6);
+		
+	}
+	public static int[] generirajTabelo(int dolzina){
+		int[] tab = new int[dolzina];
+		for(int i=0; i<tab.length; i++)
+			tab[i] = (int)(Math.random()*100000);
+		return tab;
+	}
+	public static void izpisiPrvoVrsticoTabele(int[] tab){
+		for (int i=0; i<tab.length; i++)
+			System.out.print(tab[i]+" ");
+		System.out.println();
+	}
+	public static void izpisiKazelecElemTabele(int[] tab, int index){
+		int vsota =0,i;
+		for (i=0; i<index; i++){
+			vsota= vsota + prestejStevke(tab[i])+1;
+		}
+		int dolzina = prestejStevke(tab[i+1]);
+		dolzina = (dolzina%2==0)?(int)(dolzina/2):(int)(dolzina/2)+1;
+		vsota+=dolzina;
+		System.out.printf("%"+vsota+"s", "*");
+	}
+	public static int prestejStevke(long abc){
+			int ll=1;
+			for(;(abc/=10)!=0;ll++);
+			return Math.max(1,ll);
+		}
+}
+
